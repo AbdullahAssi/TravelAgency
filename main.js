46793b3afb80d2b86c8d74cc033dfe2cf954af82
AbdullahAssi <pakabdullah225@gmail.com>
2023-05-27 02:59:04 -0700

    Added Preloader

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 // import './styles/gallery.scss';
 import './styles/style.css';
 
+
+const preloader = document.querySelector('[data-preaload]');
+
+window.addEventListener('load', function() {
+    preloader.classList.add('loaded');
+    document.body.classList.add("loaded");
+});
+
+
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
